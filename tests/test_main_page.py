07fed5b61a83c9421eafd2982918dbfc37a4d76c
Kitiketcat <supermankitiketcat@gmail.com>
2024-06-19 18:39:01 +0300

    web site and tests added

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -11,22 +11,22 @@ def test_headers(web_browser):
 
 
     elements = [
-        (page.btn_headers_home, 'HOME', '/'),
-        (page.btn_headers_new_arrivals, 'NEW ARRIVALS', '/new-arrivals'),
-        (page.btn_headers_apparel, 'APPAREL', '/apparel'),
-        (page.btn_headers_designer_collection, 'DESIGNER COLLECTION', '/designer-collection'),
-        (page.btn_headers_outerwear, 'OUTERWEAR', '/outerwear'),
-        (page.btn_headers_accessories, 'ACCESSORIES', '/accessories'),
-        (page.btn_headers_gift_card, 'GIFT CARD', '/gift-card'),
-        (page.btn_headers_shop_all, 'SHOP ALL', '/shop-all')
-                ]
+        (page.btn_headers_home, 'HOME', 'https://www.barksfifth.com/'),
+        (page.btn_headers_new_arrivals, 'NEW ARRIVALS', 'https://www.barksfifth.com/new-arrivals'),
+        (page.btn_headers_apparel, 'APPAREL', 'https://www.barksfifth.com/apparel'),
+        (page.btn_headers_designer_collection, 'DESIGNER COLLECTION', 'https://www.barksfifth.com/designer-collection'),
+        (page.btn_headers_outerwear, 'OUTERWEAR', 'https://www.barksfifth.com/outerwear'),
+        (page.btn_headers_accessories, 'ACCESSORIES', 'https://www.barksfifth.com/accessories'),
+        (page.btn_headers_gift_card, 'GIFT CARD', 'https://squareup.com/gift/MLVAZ5R6NM95E/order'),
+        (page.btn_headers_shop_all, 'SHOP ALL', 'https://www.barksfifth.com/s/shop')
+                    ]
 
     for elements_btn, elements_text, elements_link in elements:
         with allure.step(f'Проверка кнопки {elements_text}'):
             check.is_true(elements_btn.is_visible())
             check.is_true(elements_btn.is_clickable())
             check.equal(elements_btn.get_text(), elements_text)
-            check.equal(page.input_main_wrapper.get_attribute('href'), elements_link)
+            check.equal(elements_btn.get_attribute('href'), elements_link)
 
 def teat_footer(web_browser):
     """Этот тест проверяет футер главной страницы"""
@@ -34,11 +34,11 @@ def teat_footer(web_browser):
     page = MainPage(web_browser)
 
     elements = [
-        (page.btn_footers_contact, 'Contact', '/contact'),
-        (page.btn_footers_account, 'Account', '/account'),
-        (page.btn_footers_track_my_order, 'Track My Order', '/track-my-order'),
-        (page.btn_footers_privacy_policy, 'Privacy Policy', '/privacy-policy'),
-        (page.btn_footers_size_chart, 'Size Chart', '/size-chart')
+        (page.btn_footers_contact, 'Contact', 'https://www.barksfifth.com/contact'),
+        (page.btn_footers_account, 'Account', 'https://www.barksfifth.com/account'),
+        (page.btn_footers_track_my_order, 'Track My Order', 'https://www.barksfifth.com/track-my-order'),
+        (page.btn_footers_privacy_policy, 'Privacy Policy', 'https://www.barksfifth.com/privacy-policy'),
+        (page.btn_footers_size_chart, 'Size Chart', 'https://www.barksfifth.com/size-chart')
                ]
 
     for elements_btn, elements_text, elements_link in elements:
